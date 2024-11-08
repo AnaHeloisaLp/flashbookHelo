67b9241b2a2d2cacaa13caa032615778e4584517
AnaHeloisaLp <00001081279291sp@al.educacao.sp.gov.br>
2024-11-08 13:49:35 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Grupo',
+    'Em que ano o grupo BLACKPINK lançou o seu debut?',
+    'As artistas lançaram dia 8 de agosto de 2016.'
+)
+
+criaCartao(
+    'integrantes',
+    'Quem são as integrantes do BLACKPINK?',
+    'As integrantes são Jisoo, Jennie, Rosé e Lisa'
+)
+
+criaCartao(
+    'Fandom',
+    'Qual é o fandom do BLACKPINK?',
+    'O fandom se chama "Blinks".'
+)
+
+criaCartao(
+    'Musica',
+    'Qual é a música mais popular do BLACKPINK?',
+    '"Ddu-Du Ddu-Du" é uma das músicas mais populares e tem um grande número de visualizações no YouTube.'
+)
+
+criaCartao(
+    ' primeiro show ',
+    'Onde o BLACKPINK fez seu primeiro show solo?',
+    'O primeiro show solo foi em 2019, na Coreia do Sul, com a turnê "In Your Area".'
+)
+
+criaCartao(
+    'primeiro álbum',
+    'Qual foi o primeiro álbum de estúdio do BLACKPINK?',
+    'O primeiro álbum de estúdio é "The Album", lançado em 2020.'
+)
+
+criaCartao(
+    'álbum mais recente',
+    'Qual é o nome do álbum mais recente do BLACKPINK?',
+    'O álbum mais recente é "Born Pink", lançado em 2022.'
+)
+
+criaCartao(
+    'seguidores no Instagram?',
+    'Qual integrante do BLACKPINK tem o maior número de seguidores no Instagram?',
+    'Lisa é a integrante com o maior número de seguidores no Instagram.'
+)
+
+criaCartao(
+    'eality show de BLACKPINK',
+    'Qual é o nome do reality show de BLACKPINK?',
+    'O reality show se chama "Blackpink House", lançado em 2018.'
+)
